2c01c934c3e858c3cd4715b65d764e9e4f746e1b
Muhammad Shoaib <mshoaib.85@gmail.com>
2023-09-30 23:21:55 +0500

    feat math (#2)
    all in one

---
+++ math.js
@@ -0,0 +1,16 @@
+export function add(a,b){
+    //comments here
+    return a+b
+}
+
+export function div(a,b){
+    throw new Error('not implemented')
+}
+
+export function sub(a,b){
+    return a-b;
+}
+
+export function mul(a,b){
+    return a*b;
+}
